c1f76e822d4b08c0caf8ebc8562623e976397df7
Aurélie PREAUD <aureliepreaud@MacBook-Air-de-Aurelie.local>
2021-05-07 17:26:27 +0200

    suppression select

--- routes/offer.js
+++ routes/offer.js
@@ -246,7 +246,7 @@ router.get("/offer/:id", async (req, res) => {
   try {
     const offer = await Offer.findById(req.params.id).populate({
       path: "owner",
-      select: "account.username account.phone",
+      // select: "account.username account.phone",
     });
     res.json(offer);
   } catch (error) {
