aa0a66c85f9164e98c0f56e7b2e7885aab303c5c
Aurélie PREAUD <aureliepreaud@MacBook-Air-de-Aurelie.local>
2021-05-07 14:04:37 +0200

    ajout upload secPicture

--- routes/offer.js
+++ routes/offer.js
@@ -28,6 +28,8 @@ router.post("/offer/publish", isAuthenticated, async (req, res) => {
       color,
     } = req.fields;
     const picture = req.files.picture.path;
+    const secPicture = req.files.secPicture.path;
+
     if (title && price && picture) {
       //Créer la nouvelle offre
       const newOffer = new Offer({
@@ -43,12 +45,17 @@ router.post("/offer/publish", isAuthenticated, async (req, res) => {
         ],
         owner: req.user,
       });
-      //upload de l'image définie dans postman
+      //upload de l'image principale du produit définie dans postman
       const resultUpload = await cloudinary.uploader.upload(picture, {
         folder: `/vinted/offers/${newOffer._id}`,
       });
-      // Ajouter le result de l'upload à newOffer
+      //upload de l'image secondaire du produit définie dans postman
+      const resultSecUpload = await cloudinary.uploader.upload(secPicture, {
+        folder: `/vinted/offers/${newOffer._id}`,
+      });
+      // Ajouter le result des upload à newOffer
       newOffer.product_image = resultUpload;
+      newOffer.product_pictures = resultSecUpload;
       //sauver l'annonce
       await newOffer.save();
       //répondre au client
